67cb4fc292dfdd3d91200dd5270b69a0e60f7765
KanBin <kanbin@jujin8.com>
2020-12-03 15:34:35 +0800

    ViewModel的作用

--- app/src/main/java/com/sesame/learnjetpack/t1/T1ViewModel.kt
+++ app/src/main/java/com/sesame/learnjetpack/t1/T1ViewModel.kt
@@ -3,6 +3,7 @@ package com.sesame.learnjetpack.t1
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/*ViewModel被设计以感知生命周期的方式，存储和管理与UI相关的数据*/
 class T1ViewModel : ViewModel() {
     // LiveData是一个抽象类，不能直接使用。MutableLiveData是LiveData的一个最简单的实现。
     var name = MutableLiveData<String>()
