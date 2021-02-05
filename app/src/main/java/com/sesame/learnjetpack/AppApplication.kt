3a2498930208f47b9c682f4dc7f4b35af572beaf
KanBin <kanbin@jujin8.com>
2021-02-05 11:29:11 +0800

    使用ProcessLifecycleOwner监听应用程序的生命周期

--- app/src/main/java/com/sesame/learnjetpack/AppApplication.kt
+++ app/src/main/java/com/sesame/learnjetpack/AppApplication.kt
@@ -1,5 +1,13 @@
 package com.sesame.learnjetpack
 
 import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.sesame.learnjetpack.m02_lifecycle.application.ApplicationObserver
 
-class AppApplication : Application()
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(ApplicationObserver())
+    }
+}
