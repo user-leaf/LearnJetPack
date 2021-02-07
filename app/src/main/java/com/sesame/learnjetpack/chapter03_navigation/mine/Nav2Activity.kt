304491ae47d9eb94a9af7b2669c9de007305c716
KanBin <kanbin@jujin8.com>
2021-02-07 16:26:56 +0800

    复习Navigation

---
+++ app/src/main/java/com/sesame/learnjetpack/chapter03_navigation/mine/Nav2Activity.kt
@@ -0,0 +1,13 @@
+package com.sesame.learnjetpack.chapter03_navigation.mine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sesame.learnjetpack.R
+
+class Nav2Activity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav2)
+    }
+}
